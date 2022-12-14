e18030c11792835d9fd34c69b5bd37a8fe388a6a
CarlosRebollo <carlos.alrai@gmail.com>
2022-12-14 19:35:29 +0100

    enlace cambiado a HTTP

--- app/src/main/java/ies/quevedo/rpgchardatcompose/data/utils/Constants.kt
+++ app/src/main/java/ies/quevedo/rpgchardatcompose/data/utils/Constants.kt
@@ -1,6 +1,6 @@
 package ies.quevedo.rpgchardatcompose.data.utils
 
 object Constants {
-    const val BASE_URL = "https://informatica.iesquevedo.es/carlos-chat/"
+    const val BASE_URL = "http://informatica.iesquevedo.es/carlos-chat/"
     const val DB_NAME = "CharDat_database"
 }
